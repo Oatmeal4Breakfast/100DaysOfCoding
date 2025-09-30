c5d42f4d449f055b5c34ac199bb0a86c59b61fb9
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2025-09-30 14:51:04 -0400

    completed the assignment based on the spec

--- Coffee_Machine/main.py
+++ Coffee_Machine/main.py
@@ -50,7 +50,7 @@ def generate_report(resources: dict) -> list[str]:
         f"{key}: {value}ml" for key, value in resources.items() if key != "coffee"
     ]
     list_of_resources.append(f"coffee: {resources['coffee']}g")
-    list_of_resources.append(f"Money: ${resources.get('money', 0)}")
+    list_of_resources.append(f"Money: ${resources.get('money', 0.0)}")
     return list_of_resources
 
 
@@ -87,8 +87,51 @@ def check_transaction_successful(money_received: float, drink_cost: float) -> bo
         return False
 
 
+def return_change(money_received: float, drink_cost: float) -> float:
+    """returns the change to the user"""
+    return round(money_received - drink_cost, 2)
+
+
+def make_coffee(receipe: dict, resources: dict, beverage: str) -> None:
+    """Make the coffee from the receipe and deduct the required ingredients from the resources."""
+    for key, value in receipe.items():
+        resources[key] -= value
+    print(f"Here is your {beverage}. Enjoy!")
+
+
 def main():
-    print("mmmm Coffee")
+    # Make a copy of the resources to track available resources
+    available_resources = resources.copy()
+
+    while True:
+        user_choice: str = prompt_user()
+
+        # Turning off the machine if requested
+        if user_choice == "off":
+            print("Turning off the coffee machine. Goodbye!")
+            break
+        elif user_choice == "report":
+            report = generate_report(available_resources)
+            print_report(report)
+        else:
+            beverage_ingredients: dict = MENU[user_choice]["ingredients"]
+            beverage_cost: float = MENU[user_choice]["cost"]
+            if check_resources(beverage_ingredients, available_resources):
+                print(f"The cost of a {user_choice} is ${beverage_cost}.")
+                customer_payment: float = process_coins()
+                if check_transaction_successful(customer_payment, beverage_cost):
+                    change: float = return_change(customer_payment, beverage_cost)
+                    if change > 0:
+                        print(f"Here is ${change} in change.")
+                    # Add the money to the resources
+                    available_resources["money"] = (
+                        available_resources.get("money", 0.0) + beverage_cost
+                    )
+                    make_coffee(
+                        beverage_ingredients,
+                        available_resources,
+                        user_choice,
+                    )
 
 
 if __name__ == "__main__":
